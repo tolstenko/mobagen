af7ca29bb74347d3a45252662057a6b81cccf3b7
Alexandre Tolstenko <tolstenko@gmail.com>
2019-03-09 21:34:18 -0300

    Assimp via hunter #74

--- scripts/cmake-android.sh
+++ scripts/cmake-android.sh
@@ -6,8 +6,8 @@ BUILD_TYPE=${BUILD_TYPE:-Release}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $DIR/../bin-android
 cd $DIR/../bin-android
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../dependencies/polly/android-ndk-r18b-api-24-arm64-v8a-clang-libcxx11.cmake
-cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../dependencies/polly/android-ndk-r18b-api-24-arm64-v8a-clang-libcxx11.cmake
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../dependencies/polly/android-ndk-r18-api-24-arm64-v8a-clang-libcxx14.cmake
+cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=$DIR/../dependencies/polly/android-ndk-r18-api-24-arm64-v8a-clang-libcxx14.cmake
 make $*
 make android-build
 
