6758052aba498c2cb1adfd46c0d68b1040cbda7c
Marcos <marquinhoscamaleao@gmail.com>
2019-03-14 01:14:39 -0300

    Create filedialog files dependences

---
+++ engine/PathUtils.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+
+#include "Mobagen.hpp"
+
+
+namespace Mobagen
+{
+	namespace PathUtils
+	{
+		MOBAGEN_ENGINE_API void normalize(const char* path, char* out, u32 max_size);
+		MOBAGEN_ENGINE_API void getDir(char* dir, int max_length, const char* src);
+		MOBAGEN_ENGINE_API void getBasename(char* basename, int max_length, const char* src);
+		MOBAGEN_ENGINE_API void getFilename(char* filename, int max_length, const char* src);
+		MOBAGEN_ENGINE_API void getExtension(char* extension, int max_length, const char* src);
+		MOBAGEN_ENGINE_API bool hasExtension(const char* filename, const char* ext);
+		MOBAGEN_ENGINE_API bool replaceExtension(char* path, const char* ext);
+		MOBAGEN_ENGINE_API bool isAbsolute(const char* path);
+
+
+		struct MOBAGEN_ENGINE_API PathDirectory
+		{
+			explicit PathDirectory(const char* path)
+			{
+				getDir(m_dir, sizeof(m_dir), path);
+			}
+
+			operator const char*()
+			{
+				return m_dir;
+			}
+
+			char m_dir[MAX_PATH_LENGTH];
+		};
+
+
+		struct MOBAGEN_ENGINE_API FileInfo
+		{
+			explicit FileInfo(const char* path)
+			{
+				getExtension(m_extension, sizeof(m_extension), path);
+				getBasename(m_basename, sizeof(m_basename), path);
+				getDir(m_dir, sizeof(m_dir), path);
+			}
+
+			char m_extension[10];
+			char m_basename[MAX_PATH_LENGTH];
+			char m_dir[MAX_PATH_LENGTH];
+		};
+	} // namespace PathUtils
+} 
