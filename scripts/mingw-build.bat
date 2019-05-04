ffd7f4593a900612c1b02e5d1307a2b981404393
Alexandre Tolstenko <tolstenko@gmail.com>
2019-05-04 16:20:31 -0300

    path fix

--- scripts/mingw-build.bat
+++ scripts/mingw-build.bat
@@ -3,6 +3,6 @@ echo on
 where sh
 where cmake
 cmake --version
-cd ..
-cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -B%cd%\build -H%cd% -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%cd%\..\dependencies\polly\mingw-cxx17.cmake
+echo %PATH%
+cmake -DCMAKE_SH="CMAKE_SH-NOTFOUND" -B%cd%\build -H%cd% -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%cd%\dependencies\polly\mingw-cxx17.cmake
 cmake --build build
