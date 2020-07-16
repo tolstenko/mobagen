486483f985ecc38031b0b1878289d1013736fc64
Alexandre Tolstenko <tolstenko@gmail.com>
2020-07-15 22:58:59 -0300

    cmake scaffold

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,2 @@
+#hunter_config(glm VERSION 0.9.8)
+hunter_config(SDL2 URL https://github.com/InfiniBrains/SDL2/archive/v2.0.9-p1.tar.gz SHA1 57487da2bca179eb63b90ef3a36f858dd339a424)
---
+++ engine/time.hpp
@@ -0,0 +1,10 @@
+#ifndef MOBAGEN_TIME_HPP
+#define MOBAGEN_TIME_HPP
+
+
+class time {
+
+};
+
+
+#endif //MOBAGEN_TIME_HPP
