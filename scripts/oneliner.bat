4c67d968f67db9bfdeabebe194831fe8df38322e
Alexandre Tolstenko <tolstenko@gmail.com>
2021-03-03 23:39:03 -0300

    add new tools to oneliner dev tools installer

--- scripts/oneliner.bat
+++ scripts/oneliner.bat
@@ -1,12 +1,33 @@
+:: Check privileges 
+net file 1>NUL 2>NUL
+if not '%errorlevel%' == '0' (
+    powershell Start-Process -FilePath "%0" -ArgumentList "%cd%" -verb runas >NUL 2>&1
+    exit /b
+)
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install -y msys2
+:: Change directory with passed argument. Processes started with
+:: "runas" start with forced C:\Windows\System32 workdir
+cd /d %1
+
+
+powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y visualstudio2019community vscode git
+
+choco install -y visualstudio2019buildtools
+
 choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=System'
-SET "PATH=C:\tools\msys64;C:\tools\msys64\mingw64\bin;C:\tools\msys64\usr\bin;%PATH%"
-pacman -Syu
-pacman -S --noconfirm --needed mingw-w64-x86_64-toolchain mingw-w64-x86_64-glfw cmake git mingw-w64-x86_64-git-lfs
 
-cd \
-git clone --recursive https://github.com/InfiniBrains/mobagen
-cd mobagen
-sh scripts\emscripten-install.sh
+choco install -y visualstudio2019-workload-vctools --package-parameters "--includeOptional"
+
+choco install -y msys2
+
+choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=System'
+
+SET "PATH=C:\tools\msys64;C:\tools\msys64\mingw64\bin;C:\tools\msys64\usr\bin;%PATH%"
+
+pacman -Syu
+
+pacman -S --noconfirm --needed mingw-w64-x86_64-toolchain mingw-w64-x86_64-glfw python mingw-w64-x86_64-git-lfs mingw64/mingw-w64-x86_64-cmake
+
+pacman -S --noconfirm mingw64/mingw-w64-x86_64-qt5 mingw64/mingw-w64-x86_64-qt-creator
